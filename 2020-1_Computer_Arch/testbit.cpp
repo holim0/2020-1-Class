1a373429928a5edfded3795994a266e52160e02c
이희제 <heeje@huije-Mac.local>
2020-06-30 19:39:18 +0900

    Computer_Architecture

---
+++ 2020-1_Computer_Arch/testbit.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#define MAX 66000
+using namespace std;
+int arry[MAX];
+int main(){
+    
+
+    int b=4563468208;
+   
+   
+    
+    
+    
+
+    return 0;
+}
